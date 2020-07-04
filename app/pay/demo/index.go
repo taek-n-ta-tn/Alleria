e14a7bf2062b61958c206a66df0b732bfce48fbf
火星人 <450216797@qq.com>
2020-07-04 17:24:09 +0800

    引入yql

--- app/pay/demo/index.go
+++ app/pay/demo/index.go
@@ -1,8 +1,10 @@
 package demo
 
 import (
-	"github.com/taek-n-ta-tn/Alleria/lib/response"
+	"fmt"
+	"github.com/caibirdme/yql"
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/taek-n-ta-tn/Alleria/lib/response"
 )
 
 func (c *Controller) Index(r *ghttp.Request) {
@@ -15,4 +17,36 @@ func (c *Controller) Index(r *ghttp.Request) {
 	} else {
 		response.JsonOK(r, one)
 	}
+}
+
+func (c *Controller) Rule(r *ghttp.Request) {
+	rawYQL := `name='deen' and age>=23 and (hobby in ('soccer', 'swim') or score>90))`
+	ruler, _ := yql.Rule(rawYQL)
+	result, _ := ruler.Match(map[string]interface{}{
+		"name":  "deen",
+		"age":   int64(23),
+		"hobby": "soccer",
+		"score": int64(60),
+	})
+	fmt.Println(result)
+	rawYQL = `score ∩ (7,1,9,5,3)`
+	result, _ = ruler.Match(map[string]interface{}{
+		"score": []int64{3, 100, 200},
+	})
+	fmt.Println(result)
+	rawYQL = `score in (7,1,9,5,3)`
+	result, _ = ruler.Match(map[string]interface{}{
+		"score": []int64{3, 5, 2},
+	})
+	fmt.Println(result)
+	rawYQL = `score.sum() > 10`
+	result, _ = ruler.Match(map[string]interface{}{
+		"score": []int{1, 2, 3, 4, 5},
+	})
+	fmt.Println(result)
+	//Output:
+	//true
+	//true
+	//false
+	//true
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/taek-n-ta-tn/Alleria
 go 1.14
 
 require (
+	github.com/antlr/antlr4 v0.0.0-20200701161529-3d9351f61e0f // indirect
+	github.com/caibirdme/yql v0.0.0-20190801103415-238f3c90b514
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gogf/gf v1.13.0
+	github.com/stretchr/testify v1.6.1 // indirect
 )
